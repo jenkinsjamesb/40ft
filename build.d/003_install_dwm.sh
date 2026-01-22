e2ec77b417eb514f4022cac52973a1a97fa3bf7d
James Jenkins <jenkins.j.brad@gmail.com>
2026-01-22 17:12:37 -0500

    Fix build commands and add slock

--- build.d/003_install_dwm.sh
+++ build.d/003_install_dwm.sh
@@ -6,20 +6,26 @@ dnf install -y xorg-x11-server-Xorg xorg-x11-xinit
 # Build dwm
 rm -rf /tmp/dwm
 git clone https://git.suckless.org/dwm /tmp/dwm
-cd /tmp/dwm && make clean install
+(cd /tmp/dwm && make clean install)
 
 # Build dmenu
 rm -rf /tmp/dmenu
 git clone https://git.suckless.org/dmenu /tmp/dmenu
-cd /tmp/dmenu && make clean install
+(cd /tmp/dmenu && cp ./config.def.h ./config.h && sed -i -e 's/Mod1Mask/Mod4Mask/g' ./config.h) # Edit config.h to set Win as mod
+(cd /tmp/dmenu && make clean install)
+
+# Build slock
+rm -rf /tmp/slock
+git clone https://git.suckless.org/slock /tmp/slock
+(cd /tmp/slock && make clean install)
 
 # Build st
 rm -rf /tmp/st
 git clone https://git.suckless.org/st /tmp/st
-cd /tmp/st && make clean install
+(cd /tmp/st && make clean install)
 
-# Set profile to start X automatically
+# Set profile to start X on login
 echo "[[ -z \"\$DISPLAY\" && \$(tty) = /dev/tty1 ]] && startx" > /etc/profile
 
 # Start dwm on startx
-echo "exec dwm" > /home/user/.xinitrc
+echo "exec dwm" > /home/user/.xinitrc # TODO add date to status bar (https://dwm.suckless.org/status_monitor/)
