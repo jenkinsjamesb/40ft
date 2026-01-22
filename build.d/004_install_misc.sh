0e38e5b4013dab5d45a7d8c51e376195a5284fcc
James Jenkins <jenkins.j.brad@gmail.com>
2026-01-22 17:13:03 -0500

    Add programs to install

--- build.d/004_install_misc.sh
+++ build.d/004_install_misc.sh
@@ -1,2 +1,22 @@
-dnf config-manager --set-enabled crb && sudo dnf install -y epel-release
-dnf install -y tmux firefox
+# Stuff on dnf
+dnf install -y \
+tmux \
+firefox \
+stow \
+mc
+
+# Install dotfiles from repo
+(cd /home/$(cat /run/secrets/user) && \
+git clone https://github.com/jenkinsjamesb/.dotfiles.git && \
+mv .bashrc .bashrc.old && mv .bash_profile .bash_profile.old && \
+cd .dotfiles && stow .)
+
+# Install Flatpak and flathub
+dnf install -y flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Install Steam
+flatpak install -y flathub com.valvesoftware.Steam
+
+# Install FreeCAD
+flatpak install -y flathub org.freecad.FreeCAD
