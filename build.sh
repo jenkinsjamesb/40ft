818a9cd9c4774ebdda76c8a12758c50850db003d
James Jenkins <jenkins.j.brad@gmail.com>
2026-01-22 09:43:35 -0500

    Fix build command

--- build.sh
+++ build.sh
@@ -17,4 +17,4 @@ for build_file in $(find ./build.d -type f | sort); do
         echo "$RUN_PREFIX --mount=type=bind,src=./build.d,dst=./build.d bash $build_file" >> buildfile
 done
 
-sh -c "source ./config.sh && $BUILD_CMD. -f Containerfile.in -t localhost/40ft:latest"
+sh -c "$BUILD_CMD . -f Containerfile.in -t localhost/40ft:latest"
