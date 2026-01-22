49e204995e80bb36611979c173d51beec7fe2403
James Jenkins <jenkins.j.brad@gmail.com>
2026-01-22 09:44:03 -0500

    Fix user add script

--- build.d/001_add_user.sh
+++ build.d/001_add_user.sh
@@ -1,3 +1,3 @@
-adduser $(cat /run/secrets/user)
+useradd --create-home --shell /bin/bash $(cat /run/secrets/user)
 usermod --append -G wheel $(cat /run/secrets/user)
 echo "$(cat /run/secrets/user):$(cat /run/secrets/pass)" | chpasswd
