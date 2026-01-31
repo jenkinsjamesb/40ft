7d999dc36c12845e4d7f64771893f03e0d26c98d
James Jenkins <jenkins.j.brad@gmail.com>
2026-01-31 17:41:27 -0500

    Add import directory

--- build.sh
+++ build.sh
@@ -11,10 +11,10 @@ for var_name in $(cat config.sh | sed -En 's/^export (\S*)=.*$/\1 /p'); do
         RUN_PREFIX="$RUN_PREFIX --mount=type=secret,id=$var_name"
 done
 
-rm -f ./buildfile
+rm -f ./Containerfile.part
 
 for build_file in $(find ./build.d -type f | sort); do
-        echo "$RUN_PREFIX --mount=type=bind,src=./build.d,dst=./build.d bash $build_file" >> buildfile
+        echo "$RUN_PREFIX --mount=type=bind,src=./build.d,dst=/tmp/build.d --mount=type=bind,src=./import,dst=./tmp/import bash /tmp/build.d/$(basename $build_file)" >> ./Containerfile.part
 done
 
 sh -c "$BUILD_CMD . -f Containerfile.in -t localhost/40ft:latest"
